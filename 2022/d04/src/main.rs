8d2f44b5db30726f0f35bf3ae1ed9648896a09d8
Steven Black <steveb@stevenblack.com>
2022-12-03 20:53:10 -0500

    Copy the d03 testability mods to d04.

--- 2022/d04/src/main.rs
+++ 2022/d04/src/main.rs
@@ -1,23 +1,46 @@
 #![crate_name = "d04"]
-
+use std::fs;
 use common::{*};
 
 fn main() {
     println!("Advent of code 2022 day 4");
-    one();
-    two();
+    let part_one = one(read_input("input.txt"));
+    println!("Part 1 result: {}", part_one);
+
+    let part_two= two(read_input("input.txt"));
+    println!("Part 1 result: {}", part_two);
 }
 
-fn one() {
-    let input = file_to_string("input.txt".to_string());
-    let vec = input.lines().collect::<Vec<&str>>();
-
-    println!("Part 1 result: {:?}", vec);
+fn one(input: String) -> String {
+  input
 }
 
-fn two() {
-    let input = file_to_string("input.txt".to_string());
-    let vec = input.lines().collect::<Vec<&str>>();
+fn two(input: String) -> String {
+  input
+}
 
-    println!("Part 2 result: {:?}", vec);
+
+pub fn read_input(filename: &str) -> String {
+  fs::read_to_string(filename)
+      .expect("failed to read file")
+      // .lines()
+      // // .map(|line: &str| line.parse::<usize>().expect("cannot parse a usize"))
+      // .collect()
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_one() {
+    let input: String = "".to_string();
+    // assert_eq!(one(input), 157);
+  }
+
+#[test]
+  fn test_two() {
+    let input: String = "".to_string();
+    // assert_eq!(two(input), 70);
+  }
 }
